2a48823626e0587858e3278e9a44477203dd7af4
felipesantosds <felipedssantos.dev@gmail.com>
2022-02-28 15:02:53 -0300

    Add concurrency go code

---
+++ 019-concurrency/001-goroutines/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+/*
+ - Concurrency != paralelism.
+ - Paralelism:
+   - To execute one or more tasks at the same time.
+   - It's possible with CPUs with more than one nucleus.
+
+ - Concurrency
+   - Tasks can be executed in parallel (if the CPU have more than one nucleus).
+   - Can be used with CPUs with only one nucleus.
+   - One task don't need to wait other task finish to be executed.
+   - T1 → T2 → T1 → T2... (alternately)
+*/
+
+func main() {
+	go print("Hello, World!")
+	print("Programming in Go!")
+}
+
+func print(text string) {
+	for {
+		fmt.Println(text)
+		time.Sleep(time.Second)
+	}
+}
